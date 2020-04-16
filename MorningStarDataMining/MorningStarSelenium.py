48a9565c8f18c27cb622d220ef7273981d1907b9
deeplearning <manish.kumar5139@gmail.com>
2020-04-16 16:34:05 +0530

    Final getting Key Ratio for Stock

--- MorningStarDataMining/MorningStarSelenium.py
+++ MorningStarDataMining/MorningStarSelenium.py
@@ -1,8 +1,29 @@
 from selenium import webdriver
+import csv, sqlite3, pandas
 import time
 
+def getISIN(symbolname):
+    myconnection = sqlite3.connect("/home/deeplearning/Documents/PythonScripts/MiningDB.sqlite3")
+    mycursor = myconnection.cursor()
+    sqlquery = ("select ISIN from ISIN where Symbol = '%s'" %symbolname)
+    
+    mycursor.execute(sqlquery)
+    rows = mycursor.fetchall()
+    
+    for row in rows:
+        isin = row[0]
+    
+    myconnection.commit()
+    myconnection.close()
+    return isin
+
+symbolname = "ITC"
+ISIN = getISIN(symbolname)
+webquery = ("https://financials.morningstar.com/ratios/r.html?t=%s&culture=en&platform=sal" %ISIN)
+print(webquery)
+
 d = webdriver.Chrome()
-d.get('https://financials.morningstar.com/ratios/r.html?t=INE154A01025&culture=en&platform=sal')
+d.get(webquery)
 time.sleep(2)
 d.find_element_by_css_selector('.large_button').click()
 time.sleep(2)
--- MorningStarDataMining/csvtosqllite.py
+++ MorningStarDataMining/csvtosqllite.py
@@ -4,6 +4,7 @@ import pandas
 con = sqlite3.connect("/home/deeplearning/Documents/PythonScripts/MiningDB.sqlite3")
 cur = con.cursor()
 
+# Dataset from: https://www1.nseindia.com/corporates/corporateHome.html?id=equity
 df = pandas.read_csv("/home/deeplearning/Documents/PythonScripts/MorningStarDataMining/SymbolToISIN.csv")
 df.to_sql("ISIN1", con, if_exists='append', index=False)
 
